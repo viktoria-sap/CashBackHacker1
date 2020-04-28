071a3351c6f08530acb0efdd665b2091297fe38d
vika <v.sapunkova@neurobotics.ru>
2020-04-28 17:10:40 +0300

    junit4

---
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackTest {
+    @Test
+    public void shouldService100() {
+        Service service = new Service();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldService200() {
+        Service service = new Service();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
